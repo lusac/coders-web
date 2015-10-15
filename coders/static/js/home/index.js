ee49719f807bfbf46c7ccc81220a3f1c18449bc3
Raphael Brasil <raphael.brasil@corp.globo.com>
2015-10-15 20:36:56 -0300

    Refactor index.js

--- coders/static/js/home/index.js
+++ coders/static/js/home/index.js
@@ -1,2 +1,8 @@
-new TypingText(document.getElementById("intro"));
-TypingText.runAll();
+(function auto_type() {
+	new TypingText(document.getElementById("intro"));
+	TypingText.runAll();
+})();
+
+function create_room(){
+    window.location.href = '/room/create';
+}
