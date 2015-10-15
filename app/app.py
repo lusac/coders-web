5d78258c2686c9ced3fe20d1118a804d6c221260
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2015-10-15 13:21:44 -0300

    start app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == '__main__':
+    socketio.run(app)
