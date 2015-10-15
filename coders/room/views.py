bce3cdc31532d1a379d8fcac466e15abf50d8084
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2015-10-15 17:26:50 -0300

    room has been created

--- coders/room/views.py
+++ coders/room/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+import uuid
+
+from flask import Blueprint, render_template, redirect
 from flask.ext.socketio import SocketIO, emit
 
 socketio = SocketIO()
@@ -10,13 +12,22 @@ room = Blueprint(
     static_folder="static",
 )
 
-@room.route("/room")
-def index():
-    return render_template("room.html")
+
+@room.route("/room/<string:uuid>")
+def index(uuid):
+    return render_template("room.html", room_uuid=uuid)
+
+
+@room.route("/room/create")
+def create():
+    room_uuid = uuid.uuid4().hex
+    return redirect("/room/%s" % room_uuid)
+
 
 @socketio.on("connect", namespace="/socket")
 def connect():
     emit('rw', {'data': 'connected'})
+
 
 @socketio.on("broad", namespace="/socket")
 def rw(msg):
