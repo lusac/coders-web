5e0cfb68812eb18f9b51db51227e39445642038e
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2015-10-15 23:03:33 -0300

    run code inside container

--- coders/room/views.py
+++ coders/room/views.py
@@ -1,8 +1,20 @@
 import uuid
+import tarfile
+import os
 
-from flask import Blueprint, render_template, redirect, session
-from flask.ext.socketio import SocketIO, emit, join_room
+from flask import Blueprint, render_template, redirect, Response, session
 from flask import current_app
+from flask.ext.socketio import SocketIO, emit, join_room
+
+from docker import Client
+
+IMAGE_NAME = os.getenv("IMAGE_NAME", None)
+DOCKER_ADDRESS = os.getenv("DOCKER_ADDRESS", "192.168.50.4:2375")
+RUNNERS = {
+    "python": "python",
+    "nodejs": "node",
+    "ruby": "ruby",
+}
 
 socketio = SocketIO()
 
@@ -36,7 +48,39 @@ def create():
     return redirect("/room/%s" % room_uuid)
 
 
-# EVENTS
+@room.route("/room/run")
+def run(code, runner):
+    if not code:
+        return "you should send a code!", 500
+    f = open("code", 'w')
+    f.write(code)
+    f.close()
+    d = Client(base_url=DOCKER_ADDRESS)
+    test_connection = d.ping()
+    if test_connection != "OK":
+        return "docker unavaileble", 500
+    container = d.create_container(
+        image=IMAGE_NAME,
+        command="tail -f /dev/null",
+        detach=True
+    )
+    response = d.start(container)
+    with tarfile.open("code.tar.gz", "w:gz") as tar:
+        tar.add(f.name, arcname=os.path.basename(f.name))
+    t = open("code.tar.gz", "rb")
+    works = d.put_archive(container=container["Id"], path="/root", data=t)
+    t.close()
+    if not works:
+        return "Can't create file in container", 500
+    run = RUNNERS.get(runner, None)
+    if not run:
+        return "Invalid runner", 500
+    exe = d.exec_create(
+        container=container['Id'],
+        cmd="{0} /root/code".format(run)
+    )
+    gen = d.exec_start(exec_id=exe['Id'], stream=True)
+    return Response(gen, mimetype="text/plain")
 
 
 @socketio.on("joined", namespace="/socket")
@@ -44,6 +88,11 @@ def joined(msg):
     room = session.get('room')
     join_room(room)
     emit('status', {'msg': 'connected room - ' + room}, room=room)
+
+
+@socketio.on("connect", namespace="/socket")
+def connect():
+    emit('conn', {'data': 'connected'})
 
 
 @socketio.on("broad", namespace="/socket")
